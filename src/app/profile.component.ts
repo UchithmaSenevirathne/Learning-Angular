d5267e71d82e0750ab09138fa256b264e984c26c
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-06 22:42:39 +0530

    how to use components through different selectors

--- src/app/profile.component.ts
+++ src/app/profile.component.ts
@@ -2,9 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
     standalone: true,
-    selector: 'app-profile',
+    // type selector
+    // selector: 'app-profile',
+
+    // attribute selector
+    // selector: '[app-profile]',
+
+    // class selector
+    // selector: '[app-profile]:not(p)',
+
+    // multiple selector
+    selector: 'app-profile, [app-profile]',
+
     template: '<h1>Profile Component</h1>',
-    styles: ['h1 {color: green; background-color: yellow; }'],
+    styleUrls: ['./profile.component.scss']
 })
 
 export default class ProfileComponent{
