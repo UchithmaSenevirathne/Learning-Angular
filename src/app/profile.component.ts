e34e9617e850a15e2a3ba4659c3886446755f229
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-06 21:46:04 +0530

    create simple component

---
+++ src/app/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    template: '<h1>Profile Component</h1>',
+    styles: ['h1 {color: green; background-color: yellow; }'],
+})
+
+export default class ProfileComponent{
+
+}
