de504634b6ad03a563c17cc225d99e49c8ed1da5
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-07 21:21:26 +0530

    input property transforms

--- src/app/counter/Counter.component.ts
+++ src/app/counter/Counter.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input } from "@angular/core";
+import { booleanAttribute, Component, Input } from "@angular/core";
 import { changeValue, trimValue } from "./Counter.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
     standalone: true,
+    imports:[CommonModule],
     selector: 'app-counter',
     templateUrl: './Counter.component.html',
     styleUrls: ['./Counter.component.scss'],
 })
 export default class CounterComponent{
-    @Input({required: true, transform: changeValue}) count: number = 0;
+    @Input({required: true, transform: changeValue}) count: string = '';
     @Input({transform: trimValue}) message: string = '';
+    @Input({transform: booleanAttribute}) showcounter: boolean = false;
 }
