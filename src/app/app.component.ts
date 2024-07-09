9872dac654bd8d70ab7619fa51c8201176a2caf8
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-09 18:09:55 +0530

    ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import ProfileComponent from './profile.component';
 import CounterComponent from './counter/Counter.component';
@@ -11,6 +11,7 @@ import { LifeCycleHooksComponent } from './life-cycle-hooks/life-cycle-hooks.com
 import { CheckedLifeCycleComponent } from './checked-life-cycle/checked-life-cycle.component';
 import { AfterRenderComponent } from './after-render/after-render.component';
 import { ViewChildExampleComponent } from './view-child-example/view-child-example.component';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
@@ -28,18 +29,35 @@ import { ViewChildExampleComponent } from './view-child-example/view-child-examp
     CheckedLifeCycleComponent,
     AfterRenderComponent,
     ViewChildExampleComponent,
+    TaskComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  tasks = ['task 1', 'task 2', 'task 3', 'task 4']
   title = {name: 'testProject'};
   counter: number = 20;
   recieveDataFromChild = '';
   isActive = true; 
 
+  @ViewChildren(TaskComponent, {read: ElementRef}) taskComponent!: QueryList<ElementRef>;
+
   dataRecieved(data: string){
     this.recieveDataFromChild = data;
+  }
+
+  ngAfterViewInit(){
+    this.taskComponent.forEach(taskComponent => {
+      console.log(taskComponent);
+    })
+    this.taskComponent.changes.subscribe(() => {
+      console.log('child component has changed')
+    })
+
+    setTimeout(() => {
+      this.tasks.push("hi uchi");
+    }, 3000)
   }
 
   changeTitle(){
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss'
+})
+export class TaskComponent {
+  @Input() taskName!: string;
+}
