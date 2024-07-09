315b14679c1c491f0037488728a1f02f27f230fe
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-09 15:51:38 +0530

    life cycle hooks | ngAfterViewInit & ngAfterContentInit

--- src/app/life-cycle-hooks/life-cycle-hooks.component.ts
+++ src/app/life-cycle-hooks/life-cycle-hooks.component.ts
@@ -1,15 +1,22 @@
-import { Component, DestroyRef, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ContentChild, DestroyRef, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
+import ProfileComponent from '../profile.component';
 
 @Component({
   selector: 'app-life-cycle-hooks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './life-cycle-hooks.component.html',
   styleUrl: './life-cycle-hooks.component.scss'
 })
 export class LifeCycleHooksComponent {
   @Input() title = {name: ''};
   @Input() counter = 0;
+  loading = true;
+  @ContentChild(ProfileComponent) profileComponent !: ProfileComponent;
+
+  @ViewChild('uchi') uchiElement !: ElementRef;
+
   name = '';
   constructor(private destroyRef: DestroyRef){
     console.log('constructor called');
@@ -25,6 +32,8 @@ export class LifeCycleHooksComponent {
       console.log('set time out');
       this.name = this.title.name + 'added on ngOnInit'
     }, 2000);
+    this.loading = false;
+
     // this.name = this.title + 'added ngOnInit'
   }
 
@@ -39,5 +48,15 @@ export class LifeCycleHooksComponent {
 
   ngOnDestroy(){
     console.log('ngOnDestroy call')
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit call')
+  }
+
+  ngAfterViewInit(){
+    console.log(this.profileComponent)
+    console.log(this.uchiElement.nativeElement);
+    console.log('ngAfterViewInit call')
   }
 }
